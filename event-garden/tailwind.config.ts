3dfefb7f4e8a887b193bc5976685e2736510f51e
unknown <auyoncep16@gmail.com>
2024-05-15 09:31:15 +0600

    event card completed

--- event-garden/tailwind.config.ts
+++ event-garden/tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'custom-gradient': 'linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, 0.867) 90%, rgb(0, 0, 0) 100%)',
       },
     },
   },
