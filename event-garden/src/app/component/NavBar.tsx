ec60bcfb4149fb21dac9f1dc43d0abae530ec51a
unknown <auyoncep16@gmail.com>
2024-05-22 20:56:07 +0600

    sidebar & product logo  fixed

--- event-garden/src/app/component/NavBar.tsx
+++ event-garden/src/app/component/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo from '@/app/assets/logo.png'
+import { GiVineLeaf } from "react-icons/gi";
 
 type NavBarProps = {
     toggleMenu: () => void;
@@ -9,7 +9,7 @@ const NavBar:React.FC<NavBarProps> = ({toggleMenu}) => {
     
     return (<div className='flex flex-row justify-between  bg-black p-[20px] pl-0'>
         {/* <Image className='size-14 rounded-full mr-[-12px] ' src={logo} alt='logo'/> */}
-    <span className='text-[rgb(255,204,0)] text-2xl mt-1 font-[DM Sans]'>Event Garden</span>
+    <div className='flex'><GiVineLeaf style={{color:'yellow'}} className="text-4xl mr-2 mt-1"/><span className='text-[rgb(255,204,0)] text-2xl mt-1 font-[DM Sans]'>Event Garden</span></div>
     <button className='mb-10' onClick={toggleMenu} >
     <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"/></svg>
     </button>
--- event-garden/src/app/component/Sidebar.tsx
+++ event-garden/src/app/component/Sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CiCircleRemove } from "react-icons/ci";
+import { GiVineLeaf } from "react-icons/gi";
 import Link from 'next/link';
 
 type SidebarProps = {
@@ -15,9 +16,10 @@ const Sidebar:React.FC<SidebarProps> = ({isOpen, toggleMenu}) => {
     return (
     <div className={`fixed inset-0 z-50 transition-transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
         <button className='flex justify-start bg-white rounded-full mt-5 ml-5' onClick={toggleMenu}><CiCircleRemove size={24}/></button>
-        <div className='flex justify-end'><button className='flex justify-end  p-10 text-white'>Explore Events</button></div>
-        <div className='flex justify-end'><Link href="/signin" className='text-[rgb(233,186,0)] p-10 font-[DM Sans]'>Sign in</Link></div>
-        <div className='flex justify-end'><button className='flex justify-end  p-10 text-white'>Launch An Event</button></div>
+        <div className='flex justify-end p-10'><GiVineLeaf style={{color:'yellow'}} className="text-4xl mr-2 mt-1"/><button className='flex justify-end  text-[rgb(255,204,0)] text-4xl'>Event Garden</button></div>
+        <div className='flex justify-end'><button className='flex justify-end  p-10 pb-2 text-[rgb(233,186,0)] text-xl'>Explore Events</button></div>
+        <div className='flex justify-end'><Link href="/signin" className='text-[rgb(233,186,0)] p-10 pb-2 font-[DM Sans] text-xl'>Sign in</Link></div>
+        <div className='flex justify-end'><button className='flex justify-end p-10 pb-2 text-[rgb(233,186,0)] text-xl'>Launch An Event</button></div>
     </div>
     )
 }
