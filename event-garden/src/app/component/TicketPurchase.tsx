a0f90cfafd6f75d93d481712b36c436ecab0b9b1
unknown <auyoncep16@gmail.com>
2024-05-22 10:06:15 +0600

    Ticket purchase margin-padding and font style changed

--- event-garden/src/app/component/TicketPurchase.tsx
+++ event-garden/src/app/component/TicketPurchase.tsx
@@ -60,49 +60,49 @@ const TicketPurchase:React.FC<TicketPurchaseProps> = ({toggleMenu, price, quanti
                         <div className='flex justify-between'><span className='text-[rgb(250,250,252)] pt-5 pl-3'>{ticket.name}</span>
                         <span className='text-[rgb(250,250,252)] pt-5 pl-16'>{`$${ticket.price}`}</span></div>
                         <span className='text-[rgb(250,250,252)] pt-5 pl-2'>{`x${ticket.quantity}`}</span>
-                        <span className='text-[rgb(250,250,252)] pt-5 pl-10'>{`$${ticket.price*ticket.quantity}`}</span>
+                        <span className='text-[rgb(250,250,252)] pt-5 pl-10 pr-3'>{`$${ticket.price*ticket.quantity}`}</span>
                     </div>
                 )}
                     <div className='flex justify-between mt-2'>
                         <span className='text-[rgb(250,250,252)]  pl-3'>Service Charge</span>
-                        <span className='text-[rgb(250,250,252)] '>{`$${Math.round(0.15*totalPrice)}`}</span>
+                        <span className='text-[rgb(250,250,252)] pr-3'>{`$${Math.round(0.15*totalPrice)}`}</span>
                     </div>
                     <div className='flex justify-between mt-2 border-b-2 border-b-[rgb(134,135,137)] pb-4'>
                         <span className='text-[rgb(250,250,252)]  pl-3'>Processing Fee</span>
-                        <span className='text-[rgb(250,250,252)] '>{`$${Math.round(0.10*totalPrice)}`}</span>
+                        <span className='text-[rgb(250,250,252)] pr-3'>{`$${Math.round(0.10*totalPrice)}`}</span>
                     </div>
                     <div className='flex justify-between mt-2'>
                         <span className='text-[rgb(250,250,252)]  pl-3'>Total</span>
-                        <span className='text-[rgb(250,250,252)] '>{`$${totalPrice}`}</span>
+                        <span className='text-[rgb(250,250,252)] pr-3'>{`$${totalPrice + Math.round(0.15*totalPrice) + Math.round(0.10*totalPrice)}`}</span>
                     </div>
                 </div>
                 <form id="formId" className='flex flex-col' onSubmit={checkOutHandler}>
                     <div className='flex flex-col p-4 pb-0'>
                         <label className='p-4 pl-2 pb-1 text-[rgb(240,242,249)] font-[DM Sans]'>Billing Address Line 1</label>
-                        <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(134,135,137)] text-[10px] ' name="address1" type="text" placeholder='221b, Baker Street' required />
+                        <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(240,242,249)] text-[10px] ' name="address1" type="text" placeholder='221b, Baker Street' required />
                     </div>
 
                     <div className='flex flex-col p-4 pt-0'>
                         <label className=' pl-2 pb-1 text-[rgb(240,242,249)] font-[DM Sans]'>Line 2</label>
-                        <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(134,135,137)] text-[10px] ' name="address2" type="text" placeholder='221b, Baker Street' required />
+                        <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(240,242,249)] text-[10px] ' name="address2" type="text" placeholder='221b, Baker Street' required />
                     </div>
 
                     <div className='flex flex-col p-4 pt-0 pb-0'>
                         <label className=' pl-2 pb-1 text-[rgb(240,242,249)] font-[DM Sans]'>Card Number</label>
-                        <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(134,135,137)] text-[10px] ' name="cardNumber" type="text" placeholder='' required />
+                        <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(240,242,249)] text-[10px] ' name="cardNumber" type="text" placeholder='' required />
                     </div>
 
                     <div className='flex '>
                         <div className='flex flex-col p-4 pt-0 pr-1 w-[50%]'>
                             <label className=' pl-2 pb-1 text-[rgb(240,242,249)] font-[DM Sans]'>Expiration</label>
-                            <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(134,135,137)] text-[10px] ' name="expiration" type="text" placeholder='' required />
+                            <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(240,242,249)] text-[10px] ' name="expiration" type="text" placeholder='' required />
                         </div>
                         <div className='flex flex-col p-4 pt-0 pl-1 w-[50%]'>
                             <label className=' pl-2 pb-1 text-[rgb(240,242,249)] font-[DM Sans]'>CVC</label>
-                            <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(134,135,137)] text-[10px] ' name="CVC" type="text" placeholder='' required />
+                            <input className='p-3 pl-2 rounded-lg mb-4 ml-1 w-[100%] bg-[rgb(43,44,44)] text-[rgb(240,242,249)] text-[10px] ' name="CVC" type="text" placeholder='' required />
                         </div>
                     </div>
-                    <button className='flex justify-center items-center p-2 bg-[rgb(233,186,0)] text-black mt-5 rounded-[50px] w-[100%] ' type='submit'>Checkout</button>
+                    <div className='w-[100%] px-3'><button className='flex justify-center items-center p-2 bg-[rgb(233,186,0)] text-black mt-5 rounded-[50px] w-[100%] ' type='submit'>Checkout</button></div>
                 </form>
                 {  credentialValid === 0 ? <></> 
             : <p className='flex justify-center pt-7 text-[rgb(233,186,0)] text-xl '>{messageMap[credentialValid]}</p> }
