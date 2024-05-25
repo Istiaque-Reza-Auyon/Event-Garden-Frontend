5f6b2e9ccd09ef4d4f961d1dc4cd3f88c968bfbd
unknown <auyoncep16@gmail.com>
2024-05-25 23:53:21 +0600

    fixed view recent orders in event edit page

--- event-garden/src/app/edit/event/[eventId]/page.tsx
+++ event-garden/src/app/edit/event/[eventId]/page.tsx
@@ -66,7 +66,7 @@ const page:React.FC<pageProps> = () => {
         </div>
         <div className='flex justify-between'><h2 className='text-[rgb(235,239,245)] text-3xl p-2 max-w-[40%]'>Recent Orders</h2><Link href={`/order/${eventId}`} className='max-w-[40%]'><button type="submit" className='flex justify-center items-center p-3 w-[100%] bg-[rgb(255,204,0)] text-sm  text-black my-8 rounded-[50px] '>View All Orders</button></Link></div>
         <div className='flex flex-col items-center p-10 px-5 mx-4 w-[100%] border-2 shadow-3xl bg-[rgb(21,22,24)] border-black rounded-lg mb-2'>
-        {eventObject?.users?.slice(0,4).map((user:any) => <div className='flex flex-col items-center p-5 mx-4 w-[100%] max-h-[30%] border-2 bg-black border-black rounded-lg mb-2'>
+        {eventObject?.users?.slice(0,4).map((user:any) => <div className='flex flex-col items-center p-5 mx-4 w-[100%] border-2 bg-black border-black rounded-lg mb-2'>
             <div className='flex w-[100%] items-center justify-between h-[100%]'><h2 className='text-[rgb(255,204,0)] underline underline-offset-1 text-xl'>{user.name}</h2><div className='text-[rgb(136,130,121)] underline underline-offset-1 text-xs p-2'>Order #{user.orderId}</div></div>
             <div className='flex w-[100%] items-center justify-between'>
               <div className='text-white text-sm p2'>{changeDateFormat(user.createdAt)}</div>
